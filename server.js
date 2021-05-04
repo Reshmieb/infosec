16120dda5c62897570c977c525b675805018c071
reshmi singh <reshmi.s@engineerbabu.in>
2021-05-04 16:17:58 +0530

    git puch

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.get('/app-info', function(req, res) {
   res.json({headers: hObj, appStack: appMainRouteStack });
 });
 
-app.get('/package.json', function(req, res, next) {
+app.get('/_api/package.json', function(req, res, next) {
 	    fs.readFile(__dirname + '/package.json', function(err, data) {
 	      if(err) return next(err);
 	      res.type('txt').send(data.toString());
